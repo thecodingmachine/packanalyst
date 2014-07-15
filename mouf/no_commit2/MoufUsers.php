6d1553a499aa50c24dfb4f6effde377379aba9c0
David Négrier <d.negrier@thecodingmachine.com>
2014-07-15 23:47:33 +0200

    Fixing nocommit files

--- mouf/no_commit2/MoufUsers.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * This contains the users allowed to access the Mouf framework.
- */
-$users['admin'] = array('password'=>'f559d5341eeba0005ef066defb69727ac26b6c5a', 'options'=>null);
-		
-?>
