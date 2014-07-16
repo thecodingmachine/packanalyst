fd607d4aa1725e18b87672a2190e486913f0257b
David Négrier <d.negrier@thecodingmachine.com>
2014-07-16 16:28:33 +0200

    Ensuring phpDoc is UTF8

--- src/Mouf/Packanalyst/Services/StoreInDbNodeVisitor.php
+++ src/Mouf/Packanalyst/Services/StoreInDbNodeVisitor.php
@@ -44,7 +44,7 @@ class StoreInDbNodeVisitor extends NodeVisitorAbstract
 			$item['name'] = $itemName;
 			$comment = $node->getDocComment();
 			if ($comment) {
-				$item['phpDoc'] = $node->getDocComment()->getText();
+				$item['phpDoc'] = $this->ensureUtf8($node->getDocComment()->getText());
 			}
 			
 			$item['packageName'] = $this->package['packageName'];
@@ -80,5 +80,21 @@ class StoreInDbNodeVisitor extends NodeVisitorAbstract
 			
 			$this->itemDao->save($item);
 		}
+	}
+	
+	private function ensureUtf8($str) {
+		if (preg_match('%^(?:
+      [\x09\x0A\x0D\x20-\x7E]            # ASCII
+    | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+    | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+    | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+    | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+    | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+    | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+    | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+)*$%xs', $str))
+			return $str;
+		else
+			return iconv('CP1252', 'UTF-8', $str);
 	}
 }
