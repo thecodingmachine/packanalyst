08663ad127df79d11a2b7d4c618fa57e1a55c087
David Négrier <d.negrier@thecodingmachine.com>
2014-12-30 17:47:11 +0100

    Preventing default action on view more packages link

--- src/views/classAnalyzer/classAnalyzer.js
+++ src/views/classAnalyzer/classAnalyzer.js
@@ -2,5 +2,6 @@ $(function() {
 	$('a.otherpackageslink').click(function() {
 		$(this).parent('.otherpackagescontainer').find('.otherpackages').show();
 		$(this).hide();
+		return false;
 	});
 });
